962f62810d137f012c85b89f3f74cd8c7e8290f1
SakifKhan98 <sakifkhan98@gmail.com>
2020-10-05 07:40:54 +0600

    Backend Functionalities added

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i4bni.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello There! Welcome to Volunteer Network Server");
+});
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const userActivitiesCollection = client
+    .db("volunteerNetwork")
+    .collection("userActivities");
+  const eventsCollection = client.db("volunteerNetwork").collection("events");
+  //   console.log("Database connected");
+
+  app.post("/addActivity", (req, res) => {
+    const events = req.body;
+    eventsCollection.insertOne(events).then((result) => {
+      console.log(result.insertedCount);
+      res.send(result.insertedCount);
+    });
+  });
+
+  app.get("/events", (req, res) => {
+    eventsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.post("/addEvent", (req, res) => {
+    const events = req.body;
+    eventsCollection.insertOne(events).then((result) => {
+      console.log(result.insertedCount);
+      res.send(result.insertedCount);
+    });
+  });
+
+  app.post("/addUserActivity", (req, res) => {
+    const userActivity = req.body;
+    userActivitiesCollection.insertOne(userActivity).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/userActivities", (req, res) => {
+    userActivitiesCollection
+      .find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.get("/activityList", (req, res) => {
+    userActivitiesCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  //   app.get("/volunteerRegister/:id", (req, res) => {
+  //     eventsCollection.find({ id: req.params.id }).toArray((err, documents) => {
+  //       res.send(documents[0]);
+  //     });
+  //   });
+});
+
+app.listen(port);

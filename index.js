54db2dde99fddef6d7494079d72ab0057575f3fb
SakifKhan98 <sakifkhan98@gmail.com>
2020-10-05 21:19:45 +0600

    added submit redirection

--- index.js
+++ index.js
@@ -71,6 +71,16 @@ client.connect((err) => {
     });
   });
 
+  app.delete("/selectedActivities/:id", (req, res) => {
+    // console.log(req.params.id);
+    userActivitiesCollection.remove({ id: req.params.id }).then((result) => {
+      res.status(200).json({
+        message: "Delete Successfully",
+        deletedData: result,
+      });
+    });
+  });
+
   //   app.get("/volunteerRegister/:id", (req, res) => {
   //     eventsCollection.find({ id: req.params.id }).toArray((err, documents) => {
   //       res.send(documents[0]);
